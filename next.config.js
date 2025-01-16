9dd811e65d8c0e09f8a23a93403b0b48a92dea13
duquelf90 <duquekun@gmail.com>
2025-01-15 19:06:20 -0600

    update next confgi

--- next.config.js
+++ next.config.js
@@ -2,8 +2,27 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*/,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'pages',
+        expiration: {
+          maxEntries: 100,
+          maxAgeSeconds: 60 * 60 * 24 * 30, // 30 Days
+        },
+      },
+    },
+  ],
 });
 
 module.exports = withPWA({
   reactStrictMode: true,
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    return config;
+  },
 });
